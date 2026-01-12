71866cf0189ef7e40e99c46bbf39d6de062bc338
oyumusak <omer_9826@outlook.com>
2026-01-12 16:47:38 +0300

    test

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"context"
-	"fmt"
 	"os"
 	"os/signal"
 	"syscall"
@@ -10,58 +9,42 @@ import (
 	apifulrest "github.com/oyumusak/apifulrest/apifulRest"
 )
 
-var requestCount int
-
-func yusufFunc(body string, headers map[string]string) interface{} {
-
-	return map[string]interface{}{
-		"name": "omer",
-	}
-}
-
-func aliFunc(body string, headers map[string]string) interface{} {
-	if _, ok := headers["id"]; !ok {
-		return map[string]interface{}{
-			"error": "need jwt auth",
-		}
-	}
-
-	header := "hello"
-	if val, ok := headers["ip"]; ok {
-		header = val
-	}
-	return map[string]interface{}{
-		"name":   "ali",
-		"header": header,
-	}
-}
-
-func tokenValidator(headers map[string]string) bool {
-	val, ok := headers["authorizon"]
-	if !ok {
-		return false
-	}
-	headers["id"] = val
-	return true
-}
-
 func main() {
-
-	requestCount = 0
 	ctx, cancel := context.WithCancel(context.Background())
 	sigCh := make(chan os.Signal, 1)
 	signal.Notify(sigCh, os.Interrupt, syscall.SIGTERM)
 
 	server := apifulrest.New(3256)
 
-	server.AddGet("/yusuf", yusufFunc, nil)
-	server.AddGet("/ali", aliFunc, tokenValidator)
+	// Simple GET endpoint
+	server.AddGet("/hello", func(body string, headers map[string]string) interface{} {
+		return map[string]interface{}{
+			"message": "Hello, World!",
+		}
+	}, nil)
+
+	// GET endpoint with authentication decorator
+	server.AddGet("/protected", protectedHandler, authMiddleware)
 
 	go func() {
 		<-sigCh
-		fmt.Println("Kapatılıyor...")
 		cancel()
 	}()
 
 	server.Run(ctx)
+}
+
+func protectedHandler(body string, headers map[string]string) interface{} {
+	return map[string]interface{}{
+		"user": headers["id"],
+	}
+}
+
+func authMiddleware(headers map[string]string) bool {
+	token, ok := headers["Authorization"]
+	if !ok {
+		return false
+	}
+	headers["id"] = token
+	return true
 }
